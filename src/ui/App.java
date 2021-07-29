461bfc118f378ede7989d01e935e2996e1132335
Calen Jackman <calenjackman1@gmail.com>
2021-07-29 18:45:04 -0400

    update App.java to display board

--- src/ui/App.java
+++ src/ui/App.java
@@ -10,5 +10,7 @@ public class App
 	public static void main(String[] args)
 	{
 		CatanWindow catanWindow = new CatanWindow();
+		catanWindow.setDefaultCloseOperation(CatanWindow.EXIT_ON_CLOSE);
+		catanWindow.setVisible(true);
 	}
 }
